10a8e164b6f11ce0d8325f1628bc3abdfb10704c
Brent <zunyibrent@github.com>
2022-04-10 16:40:17 -0400

    lines and colors

--- js/index.js
+++ js/index.js
@@ -1,24 +1,40 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-camera.position.z = 5;
+const material2 = new THREE.LineBasicMaterial({ color: 0x0000ff });
+const points = [];
+points.push(new THREE.Vector3(-1, 0, 0));
+points.push(new THREE.Vector3(0, 1, 0));
+points.push(new THREE.Vector3(1, 0, 0));
+points.push(new THREE.Vector3(0, -1, 0));
+points.push(new THREE.Vector3(-1, 0, 0));
+const geometry2 = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(geometry2, material2);
+
+scene.add(line);
+
+camera.position.set(0, 0, 5);
 
 var animate = function () {
-	requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 };
 
 animate();
